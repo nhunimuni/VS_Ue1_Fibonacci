af48e223eb9fb4be66437bc17acddd5f34e5afa5
BabySamree <babysamree@outlook.com>
2017-12-08 18:28:35 +0100

    ue3 server kommentiert

--- src/pinnwand/server/Server.java
+++ src/pinnwand/server/Server.java
@@ -14,20 +14,19 @@ public class Server {
     public static void main(String[] args) {
 
         try {
-            //erstellt fÃ¼r die Pinnwand Anwendung diesen Port (vom localhost)
+            //erstellt für die Pinnwand Anwendung diesen Port (vom localhost)
             LocateRegistry.createRegistry(1099); //geht nur mit 1099
             Pinnwand pinnwand = new Pinnwand(); //neue Pinnwand
 
             Registry registry = LocateRegistry.getRegistry(); //speichert die Verbindung lokal ab
             registry.rebind(NAME_OF_SERVICE, pinnwand); //bindet die Pinnwand an diesen Namensdienst
 
+            System.out.println("Pinnwand läuft jetzt auf dem Port '1099' unter dem Namensdienst '" + NAME_OF_SERVICE + "'");
         } catch (RemoteException e) {
             //Exception wird geworfen, wenn Registry nicht erstellt werden konnte
             //Port ist besetzt oder falsche Portnummer angegeben
-            System.err.println("Wups, was schief gelaufen...");
+            System.err.println("Registry konnte nicht erstellt werden.");
         }
-        System.out.println("Pinnwand-Service is running.");
-
     }
 
 }
